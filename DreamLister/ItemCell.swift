c444b672776fbeb3297da8089ff786109bd72280
wyodoodoyw <mwood051@gmail.com>
2016-10-31 17:34:25 -0400

    set up tableView

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Matthew Wood on 2016-10-31.
+//  Copyright © 2016 Matthew. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+}
